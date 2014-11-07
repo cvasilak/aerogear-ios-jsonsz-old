23fd9a6a0c6938f1ae29085de253222438e1cab7
Christos Vasilakis <cvasilak@gmail.com>
2014-11-07 14:43:10 +0200

    added toJSON tests and refactor existing ones

--- AeroGearJsonSZTests/AeroGearJsonSZTests.swift
+++ AeroGearJsonSZTests/AeroGearJsonSZTests.swift
@@ -20,109 +20,188 @@ import XCTest
 import AeroGearJsonSZ
 
 class AeroGearJsonSZTests: XCTestCase {
-    let serializer = JsonSZ()
+    
+    var serializer: JsonSZ!
+    
     override func setUp() {
         super.setUp()
+        
+        serializer = JsonSZ()
     }
     
     override func tearDown() {
         super.tearDown()
     }
     
-    func testParseFromJSON() {
-        let json: String = "{\n" +
-            "    \"id\": \"b55f515be0b1d40aba01\",\n" +
-            "    \"created_at\": \"2014-10-30T09:51:24Z\",\n" +
-            "    \"updated_at\": \"2014-10-30T09:51:24Z\",\n" +
-            "    \"public\": true,\n" +
-            "    \"description\": \"a test gist\",\n" +
-            "    \"url\": \"https://api.github.com/gists/c5af515be0b1d40aba01\",\n" +
-            "    \"files\": [\n" +
-            "        {\n" +
-            "            \"filename\": \"foo.diff\",\n" +
-            "            \"type\": \"text/plain\",\n" +
-            "            \"size\": 1234\n" +
-            "        },\n" +
-            "        {\n" +
-            "            \"filename\": \"bar.diff\",\n" +
-            "            \"type\": \"text/plain\",\n" +
-            "            \"size\": 4321\n" +
-            "        }\n" +
-            "    ],\n" +
-            "    \"owner\": {\n" +
-            "        \"id\": 1,\n" +
-            "        \"login\": \"auser\",\n" +
-            "        \"repos_url\": \"https://api.github.com/users/auser/repos\",\n" +
-            "        \"site_admin\": false\n" +
-            "    }\n" +
-        "}";
+    func testJSONToObjectModelWithPrimitiveAttributes() {
+        var employeeJSON = ["id": 100, "firstname": "John", "lastname": "Doe", "title": "Software Engineer", "salary": 123.50, "married": true, "weight": 60.2]
+        // serialize from json
+        let employee:Employee = self.serializer.fromJSON(employeeJSON, to: Employee.self)
         
-        let gist:Gist = self.serializer.fromJSON(json, to: Gist.self)
-        
-    }
-    
-    func testJsonToObjectModelWithOptionalSimplePrimitivettributes() {
-        var json: [String: AnyObject] = ["first_name" : "lucy", "last" : "smith"]
-        let name:Name = self.serializer.fromJSON(json, to: Name.self)
-        XCTAssert(name.description == "firstname: lucy,lastname: smith", "from json to object model for simple json")
-        
-        let obj = self.serializer.toJSON(name)
-        //TODO make it pass
-        //XCTAssert(obj.description == "firstname: lucy, lastname: smith", "")
+        // assert construction has succeeded on primitive fields
+        XCTAssertTrue(employee.id == 100)
+        XCTAssertTrue(employee.firstname == "John")
+        XCTAssertTrue(employee.lastname == "Doe")
+        XCTAssertTrue(employee.title == "Software Engineer")
+        XCTAssertTrue(employee.salary == 123.50)
+        XCTAssertTrue(employee.married == true)
+        XCTAssertTrue(employee.weight == 60.2)
     }
 
-    func testOneToOneRelationshipFromJson() {
-        var nameJson: [String: AnyObject] = ["first_name": "lucy", "last": "smith"]
-        var buddyJson: [String: AnyObject] = ["name": nameJson]
+    func testObjectModelToJSONWithPrimitiveAttributes() {
+        // construct object
+        let employee = Employee()
+        employee.id = 100
+        employee.firstname = "John"
+        employee.lastname = "Doe"
+        employee.title = "Software Engineer"
+        employee.salary = 123.50
+        employee.married = true
+        employee.weight = 60.2
         
-        let buddy = self.serializer.fromJSON(buddyJson, to: Buddy.self)
-        XCTAssert(buddy.description == "name: firstname: lucy,lastname: smith", "from json to object model for one to one relationship")
+        // serialize to json
+        let json = self.serializer.toJSON(employee)
+        
+        // assert json dictionary has been populated
+        XCTAssertTrue(json["id"] as Int == 100)
+        XCTAssertTrue(json["firstname"] as String == "John")
+        XCTAssertTrue(json["lastname"] as String == "Doe")
+        XCTAssertTrue(json["title"] as String == "Software Engineer")
+        XCTAssertTrue(json["salary"] as Double == 123.50)
+        XCTAssertTrue(json["married"] as Bool == true)
+        XCTAssertTrue(json["weight"] as Float == 60.2)
     }
     
-    func testOptionalMissingLastName() {
-        var nameJson: [String: AnyObject] = ["first_name": "lucy"]
+     func testJSONToObjectModelWithPrimitiveAttributesAndMissingValues() {
+        var employeeJSON = ["id": 100, "title":  "Software Engineer"]
+        // serialize from json
+        let employee:Employee = self.serializer.fromJSON(employeeJSON, to: Employee.self)
         
-        let name = self.serializer.fromJSON(nameJson, to: Name.self)
-        XCTAssert(name.description == "firstname: lucy,", "from json to object model with optional values missing")
+        // assert construction has succeeded and missing values are nil
+        XCTAssertTrue(employee.id == 100)
+        XCTAssertTrue(employee.firstname == nil)
+        XCTAssertTrue(employee.lastname == nil)
+        XCTAssertTrue(employee.title == "Software Engineer")
+        XCTAssertTrue(employee.salary == nil)
+        XCTAssertTrue(employee.married == nil)
+        XCTAssertTrue(employee.weight == nil)
     }
     
-    func testOptionalMissingLastNameForBuddy() {
-        var nameJson: [String: AnyObject] = ["first_name": "lucy"]
-        var buddyJson: [String: AnyObject] = ["name": nameJson]
+    func testObjectModelToJSONWithPrimitiveAttributesAndMissingValues() {
+        // construct object
+        let employee = Employee()
+        employee.id = 100
+        employee.title = "Software Engineer"
         
-        let buddy = self.serializer.fromJSON(buddyJson, to: Buddy.self)
-        XCTAssert(buddy.description == "name: firstname: lucy,", "from json to object model with optional values missing")
+        // serialize to json
+        let json = self.serializer.toJSON(employee)
+        
+        // assert json dictionary has been populated
+        XCTAssertTrue(json["id"] as? Int == 100)
+        XCTAssertTrue(json["firstname"] == nil)
+        XCTAssertTrue(json["lastname"] == nil)
+        XCTAssertTrue(json["title"] as? String == "Software Engineer")
+        XCTAssertTrue(json["salary"]  == nil)
+        XCTAssertTrue(json["married"] == nil)
+        XCTAssertTrue(json["weight"] == nil)
     }
     
-    func testOptionalMissingNameForBuddy() {
-        var buddyJson: [String: AnyObject] = [:]
+    func testOneToOneRelationshipFromJSON() {
+        var addressJSON = ["street": "Buchanan Street", "poBox": 123, "city": "Glasgow", "country": "UK"]
+        var employeeJSON = ["firstname": "John", "address": addressJSON]
         
-        let buddy = self.serializer.fromJSON(buddyJson, to: Buddy.self)
-        XCTAssert(buddy.description == "", "from json to object model with optional values missing")
+        // serialize from json
+        let employee:Employee = self.serializer.fromJSON(employeeJSON, to: Employee.self)
+        
+        // assert construction has succeeded
+        XCTAssertTrue(employee.firstname == "John")
+        // asssert relationship has succeeded
+        XCTAssertTrue(employee.address?.street == "Buchanan Street")
+        XCTAssertTrue(employee.address?.poBox == 123)
+        XCTAssertTrue(employee.address?.city == "Glasgow")
+        XCTAssertTrue(employee.address?.country == "UK")
     }
     
-    func testOneToOneRelationshipToJson() {
-        let lucy = Buddy()
-        lucy.name = Name()
-        lucy.name?.firstname = "lucy"
-        lucy.name?.lastname = "smith"
-        let serializer = JsonSZ()
+    func testOneToOneRelationshipToJSON() {
+        // construct objects
+        let address = Address()
+        address.street = "Buchanan Street"
+        address.poBox = 123
+        address.city = "Glasgow"
+        address.country = "UK"
         
-        let buddy = serializer.toJSON(lucy)
-        //TODO make it pass
-        //XCTAssert(buddy.description == "...", "")
+        let employee = Employee()
+        employee.firstname = "John"
+        // assign relationship
+        employee.address = address
+        
+        // serialize to json
+        let json = self.serializer.toJSON(employee)
+
+        // assert construction has succeeded
+        XCTAssertTrue(json["firstname"] as String == "John")
+        // asssert relationship has succeeded
+        let addressJSON = json["address"] as [String: AnyObject]
+        XCTAssertTrue(addressJSON["street"] as String == "Buchanan Street")
+        XCTAssertTrue(addressJSON["poBox"] as Int == 123)
+        XCTAssertTrue(addressJSON["city"] as String == "Glasgow")
+        XCTAssertTrue(addressJSON["country"] as String == "UK")
     }
     
-    func testOneToManyRelationshipFromJson() {
-        var nameJson: [String: AnyObject] = ["first_name": "lucy", "last": "smith"]
-        var buddyJson: [String: AnyObject] = ["name": nameJson]
-        var teamJson: [String: AnyObject] = ["team-name": "slackers", "team-members": [buddyJson]]
+    func testOneToManyRelationshipFromJSON() {
+       // construct objects
+        var employeeAJSON = ["id": 100, "firstname": "John"]
+        var employeeBJSON = ["id": 101, "firstname": "Maria"]
         
-        let team = self.serializer.fromJSON(teamJson, to: Team.self)
-        XCTAssert(team.description == "team-name: slackers, team-members: [name: firstname: lucy,lastname: smith]", "from json to object model for one to many relationship")
+        var teamJSON = ["name": "AeroGear", "employees": [employeeAJSON, employeeBJSON]]
+        
+        // serialize from json
+        let team:Team = self.serializer.fromJSON(teamJSON, to: Team.self)
+        
+        // assert construction has succeeded
+        XCTAssertTrue(team.name == "AeroGear")
+        // asssert one-to-many relationship has succeeded
+        XCTAssertTrue(team.employees?.count == 2)
+        
+        // verify each object
+        let employeeA = team.employees?[0]
+        XCTAssertTrue(employeeA?.id == 100)
+        XCTAssertTrue(employeeA?.firstname == "John")
+        
+        let employeeB = team.employees?[1]
+        XCTAssertTrue(employeeB?.id == 101)
+        XCTAssertTrue(employeeB?.firstname == "Maria")
     }
     
-    func testOneToManyRelationshipToJson() {
-        //TODO
+    func testOneToManyRelationshipToJSON() {
+        // construct objects
+        let employeeA = Employee()
+        employeeA.id = 100
+        employeeA.firstname = "John"
+        
+        let employeeB = Employee()
+        employeeB.id = 101
+        employeeB.firstname = "Maria"
+        
+        let team = Team()
+        team.name = "AeroGear"
+        // assign relationship
+        team.employees = [employeeA, employeeB]
+        
+        // serialize to json
+        let json = self.serializer.toJSON(team)
+        
+        // assert construction has succeeded
+        XCTAssertTrue(json["name"] as String == "AeroGear")
+        // asssert relationship has succeeded
+        let employeesJSON = json["employees"] as [[String: AnyObject]]
+
+        let employeeAJSON = employeesJSON[0] as [String: AnyObject]
+        XCTAssertTrue(employeeAJSON["id"] as Int == 100)
+        XCTAssertTrue(employeeAJSON["firstname"] as String == "John")
+
+        let employeeBJSON = employeesJSON[1] as [String: AnyObject]
+        XCTAssertTrue(employeeBJSON["id"] as Int == 101)
+        XCTAssertTrue(employeeBJSON["firstname"] as String == "Maria")
     }
 }
---
+++ AeroGearJsonSZTests/Model/Model.swift
@@ -0,0 +1,73 @@
+/*
+* JBoss, Home of Professional Open Source.
+* Copyright Red Hat, Inc., and individual contributors
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import AeroGearJsonSZ
+
+class Address: JSONSerializable {
+
+    var street: String?
+    var poBox: Int?
+    var city: String?
+    var country: String?
+    
+    required init() {}
+    
+    class func serialize(source: JsonSZ, object: Address) {
+        object.street <= source["street"]
+        object.poBox <= source["poBox"]
+        object.city <= source["city"]
+        object.country <= source["country"]
+    }
+}
+
+class Employee: JSONSerializable {
+    
+    var id: Int?
+    var firstname: String?
+    var lastname: String?
+    var title: String?
+    var salary: Double?
+    var married: Bool?
+    var weight: Float?
+    
+    var address: Address?
+    
+    required init() {}
+    
+    class func serialize(source: JsonSZ, object: Employee) {
+        object.id <= source["id"]
+        object.firstname <= source["firstname"]
+        object.lastname <= source["lastname"]
+        object.title <= source["title"]
+        object.salary <= source["salary"]
+        object.married <= source["married"]
+        object.weight <= source["weight"]
+        object.address <= source["address"]
+    }
+}
+
+class Team: JSONSerializable {
+    var name: String?
+    var employees: [Employee]?
+    
+    required init() {}
+    
+    class func serialize(source: JsonSZ, object: Team) {
+        object.name <= source["name"]
+        object.employees <= source["employees"]
+    }
+}
